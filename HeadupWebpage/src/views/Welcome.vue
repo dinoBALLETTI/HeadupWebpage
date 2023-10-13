4c3ae2b97a660f8847f24dceb94f8b147e771e66
dinoBALLETTI <db00291@hotmail.com>
2023-10-13 17:16:25 +0200

    refactor(layout): containers to component views

---
+++ HeadupWebpage/src/views/Welcome.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="header_ctnr">
+        <div class="header_txt">
+            <h1 class="header_txt1">lorem isum</h1>
+            <h3 class="header_txt2">petit lorem ipsum</h3>
+            <div class="header_btn">the button component</div>
+        </div>
+        <div class="header_img">
+            <img src="../assets/images/laptop.png">
+        </div>
+    </div>
+</template>
