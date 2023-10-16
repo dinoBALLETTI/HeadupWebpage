32d836aa3bad50a465780dc8d4392d250d4b56db
dinoBALLETTI <db00291@hotmail.com>
2023-10-16 10:50:46 +0200

    feature(component): assets download and templating

--- HeadupWebpage/src/components/Navbar.vue
+++ HeadupWebpage/src/components/Navbar.vue
@@ -2,12 +2,12 @@
 </script>
 <template>
     <div class="navframe">
-        <div class="home_btn"></div>
+        <div class="home_btn"><img src="../assets/medias/Logo.png"></div>
         <div class="burger_btn">
-            <div class="solutions_btn">solutions</div>
-            <div class="about_btn">Qui sommes nous?</div>
-            <div class="blog_btn">Notre Blog</div>
-            <div class="demo_btn">réserver une demo</div>
+            <div class="solutions_btn ctnr">solutions</div>
+            <div class="about_btn ctnr">Qui sommes nous?</div>
+            <div class="blog_btn ctnr">Notre Blog</div>
+            <div class="demo_btn ctnr">réserver une demo</div>
         </div>
     </div>
 </template>
@@ -15,10 +15,28 @@
 <style scoped>
 .navframe{
     background-color: blueviolet;
+    display: flex;
+    height: 101px;
+    padding: 0px 67.2px;
+    justify-content: space-between;
+    align-items: center;
+    flex-shrink: 0;
+    align-self: stretch; 
 }
-.sticky {
-  position: fixed;
-  top: 0;
-  width: 100%;
+
+.burger_btn {
+    display: flex;
+    width: 751px;
+    align-items: center;
+    gap: 11.594px; 
 }
+
+.ctnr {
+    font-family: Poppins;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal; 
+}
+
 </style>
--- HeadupWebpage/src/views/Home.vue
+++ HeadupWebpage/src/views/Home.vue
@@ -1,22 +1,22 @@
 <script>
-import Welcome from '../components/Welcome.vue'
-import Partners from '../components/Partners.vue'
-import Gains from '../components/Gains.vue'
-import Testimonial from '../components/Testimonial.vue'
-import Cta from '../components/Cta.vue'
-import Blog from '../components/Blog.vue'
-import FooterBlock from '../components/FooterBlock.vue'
+// import Welcome from '../components/Welcome.vue'
+// import Partners from '../components/Partners.vue'
+// import Gains from '../components/Gains.vue'
+// import Testimonial from '../components/Testimonial.vue'
+// import Cta from '../components/Cta.vue'
+// import Blog from '../components/Blog.vue'
+// import FooterBlock from '../components/FooterBlock.vue'
 import Navbar from '../components/Navbar.vue'
 
 export default {
     components: {
-        Welcome,
-        Partners,
-        Gains,
-        Testimonial,
-        Cta,
-        Blog,
-        FooterBlock,
+        // Welcome,
+        // Partners,
+        // Gains,
+        // Testimonial,
+        // Cta,
+        // Blog,
+        // FooterBlock,
         Navbar
     }
 }
