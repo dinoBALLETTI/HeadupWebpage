722b43411aa245a30eeaa138cea44f940cd4fa99
dinoBALLETTI <db00291@hotmail.com>
2023-10-17 17:37:22 +0200

    feat(Carousel): move logic into component

--- HeadupWebpage/src/components/Partners.vue
+++ HeadupWebpage/src/components/Partners.vue
@@ -1,35 +1,14 @@
 <template>
-    <div class="partners_ctnr">
-        <h2>Nos partenaires de confiance</h2>
-<div class="partners_carousel">
-
-    <div v-for="partner in partners" :key="partner">
-        <CarouselCard :partner=partner />
+<div class="partners_ctnr">
+    <h2>Nos partenaires de confiance</h2>
+    <div class="partners_carousel">
+        <Carousel />
     </div>
-        </div>
-    </div>
+</div>
 </template>
 
 <script setup>
-import CarouselCard from './shared/CarouselCard.vue';
-import bpiLogo from '@/assets/medias/bpiLogo.png';
-import esiaLogo from '@/assets/medias/esiaLogo.png';
-import esjLogo from '@/assets/medias/esjLogo.png';
-import gojoLogo from '@/assets/medias/gojoLogo.png';
-import p27Logo from '@/assets/medias/p27Logo.png';
-import ressourcupLogo from '@/assets/medias/ressourcupLogo.png';
-import ttcLogo from '@/assets/medias/ttcLogo.png';
-
-const partners = [
-    bpiLogo,
-    esiaLogo,
-    esjLogo,
-    gojoLogo,
-    p27Logo,
-    ressourcupLogo,
-    ttcLogo
-]
-
+import Carousel from '@/components/shared/Carousel.vue'
 </script>
 <style scoped>
 .partners_ctnr{
--- HeadupWebpage/src/components/shared/Carousel.vue
+++ HeadupWebpage/src/components/shared/Carousel.vue
@@ -1,7 +1,26 @@
 <template>
-    <div>the carousss</div>
+    <div v-for="partner in partners">
+        <CarouselCard :partner="partner" />
+    </div>
 </template>
 
 <script setup>
+import CarouselCard from './CarouselCard.vue';
+import bpiLogo from '@/assets/medias/bpiLogo.png';
+import esiaLogo from '@/assets/medias/esiaLogo.png';
+import esjLogo from '@/assets/medias/esjLogo.png';
+import gojoLogo from '@/assets/medias/gojoLogo.png';
+import p27Logo from '@/assets/medias/p27Logo.png';
+import ressourcupLogo from '@/assets/medias/ressourcupLogo.png';
+import ttcLogo from '@/assets/medias/ttcLogo.png';
 
+const partners = [
+    bpiLogo,
+    esiaLogo,
+    esjLogo,
+    gojoLogo,
+    p27Logo,
+    ressourcupLogo,
+    ttcLogo
+]
 </script>
