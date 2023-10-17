5dc7b279e95c17c5f1ea6522d900817774536546
dinoBALLETTI <db00291@hotmail.com>
2023-10-17 18:06:37 +0200

    feat(proposition)!: text setup, resize needed

---
+++ HeadupWebpage/src/components/Proposition.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="proposition_ctnr">
+        <h2 class="txt_ctnr1">Plus d’1 salarié sur 2 n’est pas satisfait de l’offre de formation de son entreprise.</h2>
+        <h3 class="txt_ctnr2">
+            <p>Les formations sont souvent perçues comme étant peu collaboratives, trop descendantes et décorrélées des véritables problématiques des apprenants.</p>
+            <p>50% des collaborateurs jugent que les formations proposées par l’entreprise ne couvrent pas les thématiques de développement personnel qu’ils recherchent, comme la RSE. Les soft skills, par exemple, ne semblent pas du tout être assez valorisées.</p>
+            <p>*Baromètre de la formation 2023 réalisé par edFlex</p>
+        </h3>
+    </div>
+
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.proposition_ctnr {
+    display: flex;
+    width: 1510px;
+    padding: 100px 65px;
+    flex-direction: row;
+    align-items: center;
+    gap: 83.889px;
+    background: var(--violet-darker, #300080);
+    color: #FFFFFF
+}
+.txt_ctnr1 {
+    width: 589px;
+}
+.txt_ctnr2 {
+    width: 100%
+}
+</style>
