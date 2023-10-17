b7fc38fb04663f406b0969dd9f98c7a744928bbd
dinoBALLETTI <db00291@hotmail.com>
2023-10-17 17:31:09 +0200

    feat(CarouselCard): component and props

--- HeadupWebpage/src/components/shared/CarouselCard.vue
+++ HeadupWebpage/src/components/shared/CarouselCard.vue
@@ -1,10 +1,21 @@
 <template>
-    <div class="card_ctnr">{{ partner }}</div>
-</template>
+    <div class="card_ctnr"><img :src="partner"></div>
+  </template>
 
 <script setup>
+import { defineProps } from 'vue';
+
 const props = defineProps(['partner'])
 </script>
 
 <style scoped>
+.card_ctnr {
+    width: 300px;
+    height: 300px;
+}
+.card_ctnr img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
 </style>
