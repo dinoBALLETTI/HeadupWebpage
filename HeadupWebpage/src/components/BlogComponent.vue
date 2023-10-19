4e03a678fdbef1ac6e227e6537c05aa0bc8cbd73
dinoBALLETTI <db00291@hotmail.com>
2023-10-19 14:22:18 +0200

    feat(blog)!: set elems. Post import TBD

--- HeadupWebpage/src/components/BlogComponent.vue
+++ HeadupWebpage/src/components/BlogComponent.vue
@@ -1,9 +1,79 @@
+<script setup>
+import BlogCardItem from '../components/shared/BlogCardItem.vue'
+import ButtonItem from './shared/ButtonItem.vue';
+
+const posts = [
+    {
+    source: "https://media.glamour.com/photos/637ba22fdefafdff06cebb9e/16:9/w_1280,c_limit/653486498",
+    theme: "Gestion du stress",
+    title: "Un article sur la Lune",
+    snippet: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+    time: "5 min",
+    date: "Janvier 2023"
+    },
+    {
+    source: null,    
+    theme: "Gestion du stress",
+    title: "Un article sur Mars",
+    snippet: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+    time: "5 min",
+    date: "Janvier 2023"
+    },
+    {
+    source:"https://static.nationalgeographic.fr/files/styles/image_3200/public/thumb-1570438766.jpg?w=1600",    
+    theme: "Gestion du stress",
+    title: "Un article sur Mercure",
+    snippet: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+    time: "5 min",
+    date: "Janvier 2023"
+    }
+]
+</script>
 <template>
-        <div class="blog_ctnr">
-        <h2>ressoruces utiles</h2>
-        <h3>sdjfljdsfl</h3>
-        <div class="blog_components">
-            <!-- v-for article in articles -->
+
+    <div class="main-ctnr"> 
+
+        <div class="text">
+            <h2>Nos ressources utiles</h2>
+            <h3>Des conseils sur les thématiques de la préparation mentale, du coaching et de l’accompagnement des RHs.</h3>
         </div>
+       
+        <div class="blog-ctnr">
+
+            <div class="blogSection" v-for="(post, index) in posts" :key="index">
+                
+                <BlogCardItem 
+                :source="post.source"
+                :title="post.title"
+                :snippet="post.snippet"
+                :time="post.time"
+                :date="post.date"
+                :theme="post.theme"
+                />
+            </div>
+        </div>
+
+        <ButtonItem :text="'Voir les articles'" :link="'tbd'" />
+
     </div>
+ 
 </template>
+
+<style scoped>
+.main-ctnr {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 64px;
+}
+.text {
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+}
+.blog-ctnr {
+    display: flex;
+    flex-direction: row;
+    gap: 20px
+}
+</style>
--- HeadupWebpage/src/views/Home.vue
+++ HeadupWebpage/src/views/Home.vue
@@ -5,38 +5,11 @@ import Proposition from '../components/PropositionComponent.vue'
 import Gains from '../components/GainsComponent.vue'
 import Testimonial from '../components/TestimonialComponent.vue'
 import Cta from '../components/CtaComponent.vue'
-// import Blog from '../components/BlogComponent.vue'
+import Blog from '../components/BlogComponent.vue'
 // import FooterBlock from '../components/FooterBlockComponent.vue'
 import Navbar from '../components/NavbarComponent.vue'
 
-import BlogCardItem from '../components/shared/BlogCardItem.vue'
 
-const posts = [
-    {
-    source: "https://media.glamour.com/photos/637ba22fdefafdff06cebb9e/16:9/w_1280,c_limit/653486498",
-    theme: "Gestion du stress",
-    title: "Un article sur la Lune",
-    snippet: "le bon vieux lorem ipsum",
-    time: "5 min",
-    date: "Janvier 2023"
-    },
-    {
-    source: null,    
-    theme: "Gestion du stress",
-    title: "Un article sur Mars",
-    snippet: "le bon vieux lorem ipsum",
-    time: "5 min",
-    date: "Janvier 2023"
-    },
-    {
-    source:"https://static.nationalgeographic.fr/files/styles/image_3200/public/thumb-1570438766.jpg?w=1600",    
-    theme: "Gestion du stress",
-    title: "Un article sur Mercure",
-    snippet: "le bon vieux lorem ipsum",
-    time: "5 min",
-    date: "Janvier 2023"
-    }
-]
 </script>
 
 <template>
@@ -51,17 +24,6 @@ const posts = [
     <Cta />
     <Blog />
     <FooterBlock />
-    <div class="blogSection" v-for="(post, index) in posts" :key="index">
-            
-    <BlogCardItem 
-    :source="post.source"
-    :title="post.title"
-    :snippet="post.snippet"
-    :time="post.time"
-    :date="post.date"
-    :theme="post.theme"
-    />
-    </div>
 
 </div>
 </template>
