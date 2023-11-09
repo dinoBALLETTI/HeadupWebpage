a6b3d95bcaab18800f4c8bac5f2f7992e9164956
dinoBALLETTI <db00291@hotmail.com>
2023-11-09 17:47:13 +0100

    refactor(components): position of components in App.vue

--- HeadupWebpage/src/App.vue
+++ HeadupWebpage/src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import NavHeaderComponent from './components/NavHeaderComponent.vue';
+import NavFooterComponent from './components/NavFooterComponent.vue';
+import FooterBlockComponent from './components/FooterBlockComponent.vue';
 </script>
 
 <template>
+    <NavHeaderComponent />
+    <NavFooterComponent />
+    <FooterBlockComponent />
+    
     <RouterView />
 </template>
